aff7cdda6cdd7d1512f7e79a7188a052072c9f9a
Samuel Fernandes <zamuelfernandes@gmail.com>
2022-07-06 10:10:19 -0300

    Dropdown otimizado

--- src/js/script.js
+++ src/js/script.js
@@ -9,3 +9,23 @@ $(document).ready(function () {
     });
   });
 });
+
+// EFEITO DO DROPDOWN --------------------------------
+window.addEventListener('DOMContentLoaded', () => {
+  const menuBtn = document.querySelector('#menu-btn')
+  const dropdown = document.querySelector('#dropdown')
+
+  menuBtn.addEventListener('click', () => {
+    /* if(dropdown.classList.contains('hidden')){
+        dropdown.classList.remove('hidden');
+        dropdown.classList.add('flex');
+    }else{
+        dropdown.classList.remove('flex')
+        dropdown.classList.add('hidden')
+    } */
+
+    dropdown.classList.toggle('hidden')
+    dropdown.classList.toggle('absolute')
+  })
+
+})
